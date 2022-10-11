788396d694cc935a3613347b51e782656e35827f
SamuMontoya <samu7montoya@gmail.com>
2022-10-11 16:19:00 -0500

    Comments

---
+++ components/CommentItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-container class="bg-light p-2 mt-3">
+    <b-row>
+      <b
+        ><b-col>{{ author }} - {{ authorEmail }}</b-col></b
+      >
+      <b-col class="text-right"
+        ><i>{{ new Date(created).toLocaleDateString() }}</i></b-col
+      >
+    </b-row>
+    <p class="mt-3">{{ content }}</p>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "CommentItem",
+  props: {
+    author: {
+      type: String,
+      defaut: "Anonym",
+    },
+    authorEmail: {
+      type: String,
+      default: "Unknow email",
+    },
+    content: {
+      type: String,
+      default: "",
+    },
+    created: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style>
+b {
+  color: gray;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,7 @@
       <main class="p-5">
         <b-row class="justify-content-center">
           <b-col sm="6" lg="4" v-for="article in articles" :key="article.slug">
-            <ArticleCard 
-              v-bind="article"
-            />
+            <ArticleCard v-bind="article" />
           </b-col>
         </b-row>
       </main>
@@ -20,18 +18,22 @@ export default {
   name: "IndexPage",
   data() {
     return {
-      articles: [ ],
+      articles: [],
     };
   },
   async mounted() {
-    const url = `http://localhost:9999/.netlify/functions/articles`
-    const { articles } = await this.$http.$get(url)
+    const baseUrl =
+      location.hostname === "localhost"
+        ? "http://localhost:9999"
+        : "https://samublog.netlify.app";
+    const url = `${baseUrl}/.netlify/functions/articles`;
+    const { articles } = await this.$http.$get(url);
     this.articles = articles.map((a) => ({
       ...a,
-      author: a['author-name'][0],
+      author: a["author-name"][0],
       date: new Date(a.updated),
       cover: a.cover[0]?.thumbnails.large.url,
-    }))
-  }
+    }));
+  },
 };
 </script>
